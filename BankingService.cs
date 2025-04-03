35ec4e6a5ebdf4e6d58d3482857afeb2eb421778
Torok Donat <td.zazsa@gmail.com>
2025-04-03 18:12:34 +0200

    BankingService osztály kész

---
+++ BankingService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank2
+{
+    internal abstract class BankingService
+    {
+        Owner owner;
+        public Owner Owner { get => owner; }
+
+        protected BankingService(Owner owner) { this.owner = owner; }
+    }
+}
