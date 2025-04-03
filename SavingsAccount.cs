024fd6e622688675cc42af46be450d406223afa7
Torok Donat <td.zazsa@gmail.com>
2025-04-03 18:29:45 +0200

    SavingsAccount osztály kész

---
+++ SavingsAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank2
+{
+    internal class SavingsAccount : BankAccount
+    {
+        static double baseInterest = 0.1;
+
+        public double Interest { get; set; }
+
+        public SavingsAccount(Owner owner) : base(owner)
+        {
+            Interest = baseInterest;
+        }
+
+        public override bool Withdraw(double amount)
+        {
+            if (Balance - amount >= 0)
+            {
+                Balance -= amount;
+                return true;
+            }
+            return false;
+        }
+
+        public void AddInterest()
+        {
+            Balance += Balance * (Interest / 100);
+        }
+    }
+}
