4573ab30ed70937467e37e90a478a73f9b09d261
Torok Donat <td.zazsa@gmail.com>
2025-04-03 18:26:38 +0200

    CreditAccount osztály kész

---
+++ CreditAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank2
+{
+    internal class CreditAccount : BankAccount
+    {
+        double creditLimit;
+
+        public double CreditLimit { get => creditLimit; }
+        public CreditAccount(Owner owner, double creditLimit) : base(owner)
+        {
+            this.creditLimit = creditLimit;
+        }
+
+        public override bool Withdraw(double amount)
+        {
+            if (this.Balance - amount >= CreditLimit)
+            {
+                this.Balance -= amount;
+                return true;
+            }
+            return false;
+        }
+    }
+}
