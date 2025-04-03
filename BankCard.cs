f9acf37d87b72d181ee93234fc754a6db3ff58e6
Torok Donat <td.zazsa@gmail.com>
2025-04-03 18:31:38 +0200

    BankCard osztály kész

---
+++ BankCard.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank2
+{
+    internal class BankCard : BankingService
+    {
+        double accountBalance;
+        string cardNumber;
+
+        public string CardNumber { get; }
+        public double AccountBalance { get; }
+        public BankCard(Owner owner, double accountBalance, string cardNumber) : base(owner)
+        {
+            this.accountBalance = accountBalance;
+            this.cardNumber = cardNumber;
+        }
+
+        public bool Purchase(double amount)
+        {
+            if (accountBalance - amount >= 0)
+            {
+                accountBalance -= amount;
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
